127b85dba3d40196916c54dc3447a5c8760cf7e0
Danish ijaz <dali19950@gmail.com>
2023-08-05 20:48:23 +0500

    fix issues

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const firebase = require("firebase");
 const firebaseConfig = {
-  apiKey: "AIzaSyBZVjcT1QFHyc89b4nbjcWCiXNaN5VuD_0",
-  authDomain: "foodapp-1a522.firebaseapp.com",
-  projectId: "foodapp-1a522",
-  storageBucket: "foodapp-1a522.appspot.com",
-  messagingSenderId: "1037711435770",
-  appId: "1:1037711435770:web:a3df1b8e349e91bc30e643",
+  apiKey: "AIzaSyC-w-8kIcAXsq6GujJc83dk_IHsu4ZnB-o",
+  authDomain: "foodapp-6efd1.firebaseapp.com",
+  databaseURL: "https://foodapp-6efd1-default-rtdb.firebaseio.com",
+  projectId: "foodapp-6efd1",
+  storageBucket: "foodapp-6efd1.appspot.com",
+  messagingSenderId: "993230065303",
+  appId: "1:993230065303:web:633a3d26a5f4cb2fcd63d2",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
